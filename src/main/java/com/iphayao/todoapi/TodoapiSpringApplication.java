d069cacd10f84dc3f54697b300f3ee5655418a34
iphayao <phayaob@gmail.com>
2018-08-03 21:47:06 +0700

    Initial project with Spring Boot

---
+++ src/main/java/com/iphayao/todoapi/TodoapiSpringApplication.java
@@ -0,0 +1,12 @@
+package com.iphayao.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoapiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoapiSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/iphayao/todoapi/TodoapiSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iphayao.todoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoapiSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
